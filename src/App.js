3aabd62705a4a67addc0f2acd4fface73bdf6afa
Chanran33 <k55416325@gmail.com>
2022-05-25 04:47:09 +0900

    p5.js, ml5.js 설치

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home';
-import Drawing from './pages/Drawing';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Drawing from "./pages/Drawing";
+import Drawing2 from "./pages/Drawing2";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />}></Route>
       <Route path="/drawing" element={<Drawing />}></Route>
-    </Routes> 
+      <Route path="/drawing2" element={<Drawing2 />}></Route>
+    </Routes>
   );
 };
 
